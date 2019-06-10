2dee3a3338aad8df037bd18122f9581ba1ed3dea
karenzshea <karenzshea@gmail.com>
2019-06-10 09:16:19 +0200

    better compile error

--- code-poems/unsaid.cpp
+++ code-poems/unsaid.cpp
@@ -3,19 +3,19 @@
 #include <tuple>
 #include <type_traits>
 
-template <typename T, typename enable = void> struct is : std::false_type {};
+template <typename T, typename enable = void> struct is_brave : std::false_type {};
 template <typename T>
-struct is<T, decltype(std::declval<T &>().brave_enough())> : std::true_type {};
+struct is_brave<T, decltype(std::declval<T &>().enough())> : std::true_type {};
 
 template <typename Me, typename You, typename Things>
-typename std::enable_if_t<is<Me>{}> say(Me &me, You &you, Things &toSay) {
+typename std::enable_if_t<is_brave<Me>{}> say(Me &me, You &you, Things &toSay) {
   return me >> toSay >> you;
 }
 
 struct You {};
 
 struct Me {
-  // bool brave_enough() {}
+  // bool enough() {}
 };
 
 int main() {
