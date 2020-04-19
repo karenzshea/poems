f085f511e748b693064ad2d6bd029076104ed8aa
karenzshea <karenzshea@gmail.com>
2020-04-19 16:32:43 +0200

    make work when shouldnt work anyway

--- code-poems/unsaid.cpp
+++ code-poems/unsaid.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
-#include <tuple>
 #include <type_traits>
 
-template <typename T, typename enable = void> struct is_brave : std::false_type {};
+template <typename T, typename enable = void>
+struct is_brave : std::false_type {};
+
 template <typename T>
-struct is_brave<T, decltype(std::declval<T &>().enough())> : std::true_type {};
+struct is_brave<T, decltype(std::declval<T &>().enough(), void())>
+    : std::true_type {};
 
 template <typename Me, typename You, typename Things>
 typename std::enable_if_t<is_brave<Me>{}> say(Me &me, You &you, Things &toSay) {
-  return me >> toSay >> you;
 }
 
 struct You {};
